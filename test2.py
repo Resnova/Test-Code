0a1627b6d724eb0a5f98f5d2ef19456fa08f976b
mithunmadhavan <37394814+mithunmadhavan@users.noreply.github.com>
2018-03-15 15:24:12 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,7 +9,7 @@ for x in range (1, panelSize+1):
    number.append(x)
 print "\nNon defective PCB Available\n", number
 
-error = [3,7]   #int(raw_input("Enter the defective PCB Barcode"))
+error = int(raw_input("Enter the defective PCB Barcode"))
 
 for i in error:
    number.insert(i-1,"x")
@@ -20,11 +20,11 @@ print "\nSorted Panel with Defective PCB Available\n", number
 
 barcode = int(raw_input("Please enter Barcode to scan\n"))
 print "\nYou have entered ",barcode,"\n"
-if 0<barcode<=panelSize:
+#if 0<barcode<=panelSize:
 
 
 
-   for x in range ( , ):
-      print "PCB available are\n", x+1 
-else:
-   print "!!...SORRY PCB BARCODE NOT REGISTERED...!!"
+#   for x in range ( , ):
+#      print "PCB available are\n", x+1 
+#else:
+#   print "!!...SORRY PCB BARCODE NOT REGISTERED...!!"
