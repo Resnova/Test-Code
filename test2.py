e1cf9c41f3ec2f101a76b60f8afcd3f9de905cf7
mithunmadhavan <kodothmithun@gmail.com>
2018-03-15 14:27:37 +0530

    git://github.com/Resnova/Test-Code.git

--- test2.py
+++ test2.py
@@ -7,19 +7,24 @@ print "Number of PCB in total\t\n",panelSize
 number = []
 for x in range (1, panelSize+1):
    number.append(x)
-print "\nPCB Numbers Available\n", number
+print "\nNon defective PCB Available\n", number
 
-barcode = int(raw_input('\nEnter the Barcode INPUT\n'))
+error = [3,7]   #int(raw_input("Enter the defective PCB Barcode"))
+
+for i in error:
+   number.insert(i-1,"x")
+   number.pop(panelSize)
+
+print "\nSorted Panel with Defective PCB Available\n", number
+
+
+barcode = int(raw_input("Please enter Barcode to scan\n"))
 print "\nYou have entered ",barcode,"\n"
 if 0<barcode<=panelSize:
-   temp = 0
-   newpcb=pcb
-   while barcode > pcb:
-      pcb+=newpcb
-      temp+=1
 
-   temp2=temp*newpcb
-   for x in range (temp2, temp2+newpcb):
+
+
+   for x in range ( , ):
       print "PCB available are\n", x+1 
 else:
    print "!!...SORRY PCB BARCODE NOT REGISTERED...!!"
